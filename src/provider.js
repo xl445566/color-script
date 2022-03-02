9e8524038d7055afab79871a53c911dc34510729
seodongsu <xl445566@naver.com>
2022-03-03 00:14:58 +0900

    fix: Complete lastline check

--- src/provider.js
+++ src/provider.js
@@ -3,8 +3,8 @@ const { tokenTypes, tokenModifiers } = require("./legend");
 const { checkBooleanType, checkNumberType } = require("./type");
 const { validateLine, trimBlankText, removeComment } = require("./cleanUp");
 
-const document = {};
-const helper = helpProvider();
+let document = {};
+const helper = createHelper();
 const provider = createProvider();
 
 function createProvider() {
@@ -59,16 +59,6 @@ function createProvider() {
     return result;
   }
 
-  function validateType(value) {
-    if (checkBooleanType(value)) {
-      tokenData = parseToken("boolean");
-    } else if (checkNumberType(value)) {
-      tokenData = parseToken("number");
-    }
-
-    return tokenData;
-  }
-
   function parseToken(type) {
     return {
       tokenType: "type",
@@ -86,9 +76,9 @@ function createProvider() {
         tempParsedText.value += lines[i];
 
         if (lines[i].includes(";")) {
-          const validatedTypeResult = validateType(tempParsedText.value);
+          tokenData = helper.validateType(tempParsedText.value);
 
-          if (validatedTypeResult) {
+          if (tokenData) {
             results.push({
               line: tempParsedText.line,
               startCharacter: tempParsedText.startCharacter,
@@ -102,11 +92,14 @@ function createProvider() {
           isContinue = false;
         }
 
+        helper.validateLastline(i, lines.length);
         continue;
       } else if (isContinue && !lines[i]) {
+        helper.validateLastline(i, lines.length);
         continue;
       }
 
+      // tokenData, currentOffset 초기화
       tokenData = null;
       currentOffset = 0;
 
@@ -116,6 +109,7 @@ function createProvider() {
       isCommenting = validatedLineResults.isCommenting;
 
       if (isSkip || isCommenting) {
+        helper.validateLastline(i, lines.length);
         continue;
       }
 
@@ -134,12 +128,12 @@ function createProvider() {
       const declarationArea = convertedLineArray[0].split(" ");
       const definitionArea = convertedLineArray[1].trim();
 
-      // 변수 시작 , 종료 포지션
+      // 변수 시작 , 종료 위치
       let startPos = currentOffset + declarationArea[0].length + 1;
       const endPos = startPos + declarationArea[1].length;
 
       // 변수의 값으로 Type 체크 tokenData 생성
-      validateType(definitionArea);
+      tokenData = helper.validateType(definitionArea);
 
       // Number Multiline 경우
       if (!definitionArea.includes(";")) {
@@ -149,13 +143,11 @@ function createProvider() {
         tempParsedText.value = definitionArea;
 
         isContinue = true;
-
+        helper.validateLastline(i, lines.length);
         continue;
       }
 
       // if : 변수 초기선언시 const , var , let -> parsing
-      // else if :  이미 선언된 변수 , const인 변수는 로직을 수행하지 못하도록 startPos 조정
-      // else if : 변수 = 변수를 할당할 때
       if (
         declarationArea[0] === "var" ||
         declarationArea[0] === "let" ||
@@ -180,6 +172,7 @@ function createProvider() {
         document[declarationArea[0]][0].statement !== "const" &&
         tokenData
       ) {
+        // else if :  이미 선언된 변수 , const인 변수는 로직을 수행하지 못하도록 startPos 조정
         const variableName = declarationArea[0];
         startPos = endPos - variableName.length - 1;
 
@@ -192,21 +185,33 @@ function createProvider() {
           tokenData,
         });
       } else if (document[declarationArea.slice(0, -1)] && !tokenData) {
+        // else if : 변수 = 변수를 할당할 때 -> 선언 O 변수 , 선언 X 변수
+        const variableInValue = definitionArea.slice(0, -1);
         const variableName = declarationArea[0];
-        const latestVariableInfo =
-          document[variableName][document[variableName].length - 1];
+        let latestVariableInfo = null;
 
-        startPos = endPos - variableName.length - 1;
-        tokenData = latestVariableInfo.tokenData;
+        if (document[variableInValue]) {
+          latestVariableInfo = document[variableInValue].slice(-1)[0];
+          startPos = endPos - variableName.length - 1;
+          tokenData = latestVariableInfo.tokenData;
 
-        document[variableName].push({
-          value: definitionArea,
-          line: i,
-          startPos,
-          endPos,
-          length: endPos - startPos,
-          tokenData,
-        });
+          document[variableName].push({
+            value: definitionArea,
+            line: i,
+            startPos,
+            endPos,
+            length: endPos - startPos,
+            tokenData,
+          });
+          console.log("\n");
+          console.log("variable :::::", declarationArea);
+          console.log("value :::::", definitionArea);
+          console.log("line :::::", i);
+          console.log("offset :::::", currentOffset);
+          console.log("start :::::", startPos - declarationArea[0].length);
+          console.log("end :::::", endPos);
+          console.log("tokenData :::::", tokenData);
+        }
       }
 
       if (tokenData) {
@@ -218,6 +223,8 @@ function createProvider() {
           tokenModifiers: tokenData.tokenModifiers,
         });
       }
+
+      helper.validateLastline(i, lines.length);
     }
 
     return results;
@@ -225,11 +232,36 @@ function createProvider() {
 
   return {
     provideDocumentSemanticTokens,
+    encodeTokenModifiers,
+    encodeTokenType,
+    parseText,
+    parseToken,
   };
 }
 
-function helpProvider() {
-  return {};
+function createHelper() {
+  function validateType(value) {
+    let tokenData = null;
+
+    if (checkBooleanType(value)) {
+      tokenData = provider.parseToken("boolean");
+    } else if (checkNumberType(value)) {
+      tokenData = provider.parseToken("number");
+    }
+
+    return tokenData;
+  }
+
+  function validateLastline(lineNumber, totalLength) {
+    if (lineNumber === totalLength - 1) {
+      document = {};
+    }
+  }
+
+  return {
+    validateType,
+    validateLastline,
+  };
 }
 
 module.exports = provider;
@@ -241,6 +273,3 @@ module.exports = provider;
 // console.log("offset :::::", currentOffset);
 // console.log("start :::::", startPos - declarationArea[0].length);
 // console.log("end :::::", endPos);
-// console.log("validatedTypeResult :::::", validatedTypeResult);
-// console.log("latestVariableInfo", latestVariableInfo);
-// console.log(document[declarationArea.slice(0, -1)]);
